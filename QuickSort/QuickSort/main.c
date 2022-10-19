28f8687295dd37f715e1c0964d3864506099e32a
linda2927 <linda2927@naver.com>
2022-10-19 12:08:08 +0900

    Refactor directory structure for QuickSort

--- QuickSort/QuickSort/main.c
+++ QuickSort/QuickSort/main.c
@@ -24,14 +24,14 @@ int Partition(int DataSet[], int Left, int Right)
         // 오른쪽에서 오는 요소와 만나기 직전까지
         while (DataSet[Left] <= Pivot && Left < Right)
             ++Left;
-        
+
         // 기준 요소보다 작은 요소를 만날 때까지 왼쪽으로 한칸씩 이동
         // Left 와 Right 가 나란히 있을 때 둘이 자리를 바꾼 뒤,
         // Right를 한번 더 왼쪽으로 이동하여 Left 와 포갬
         // 그 다음에 기준 요소와 Right 자리의 요소를 Swap
         while (DataSet[Right] >= Pivot && Left <= Right)
             --Right;
-        
+
         if (Left < Right)
             Swap(&DataSet[Left], &DataSet[Right]);
         else
@@ -56,7 +56,7 @@ void QuickSort(int DataSet[], int Left, int Right)
 int main( void )
 {
     int DataSet[] = {6, 4, 2, 3, 1, 5};
-    int Length = sizeof DataSet / sizeof DataSet[0];    
+    int Length = sizeof DataSet / sizeof DataSet[0];
     int i = 0;
 
     QuickSort( DataSet, 0, Length-1 );
